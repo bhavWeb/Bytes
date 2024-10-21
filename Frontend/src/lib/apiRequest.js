928cafae521d6f8be7f2a5946bc18a12aedaaa12
OlafGFG <olafgfg1@gmail.com>
2024-10-22 01:28:55 +0530

    changes env variable name specific for vite React

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -4,7 +4,7 @@ console.log("API Base URL:", process.env.NODE_ENV === 'production' ? process.env
 
 const apiRequest = axios.create({
     baseURL : process.env.NODE_ENV === 'production' 
-    ? process.env.BACKEND_URL   // Deployed backend URL (for production)
+    ? process.env.VITE_BACKEND_URL  // Deployed backend URL (for production)
     : "http://localhost:8080",  // Local development URL  
     withCredentials : true,
 });
