3a8f69618691576fd7063db46184c0e691a89864
OlafGFG <olafgfg1@gmail.com>
2024-10-22 01:17:56 +0530

    debugging log code for apiurl testing

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+console.log("API Base URL:", process.env.NODE_ENV === 'production' ? process.env.BACKEND_URL : "http://localhost:8080");
+
 const apiRequest = axios.create({
     baseURL : process.env.NODE_ENV === 'production' 
     ? process.env.BACKEND_URL   // Deployed backend URL (for production)
