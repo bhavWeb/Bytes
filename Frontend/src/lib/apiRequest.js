8a00fcd4be52d6457be3de0b9f47b03a641b85b8
OlafGFG <olafgfg1@gmail.com>
2024-10-22 01:39:05 +0530

    HARDCODED The Backend URL in ApiRequest

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-console.log("API Base URL:", process.env.NODE_ENV === 'production' ? process.env.BACKEND_URL : "http://localhost:8080");
+console.log("API Base URL:", process.env.NODE_ENV === 'production' ? "https://bytes-backend-d56w.onrender.com" : "http://localhost:8080");
 
 const apiRequest = axios.create({
     baseURL : process.env.NODE_ENV === 'production' 
-    ? process.env.VITE_BACKEND_URL  // Deployed backend URL (for production)
+    ? "https://bytes-backend-d56w.onrender.com"  // Deployed backend URL (for production)
     : "http://localhost:8080",  // Local development URL  
     withCredentials : true,
 });
